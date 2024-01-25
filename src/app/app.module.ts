c6f2e0fb18e51ab6800c3811ac017c97c1066ccb
bollo18 <bolloo18@gmail.com>
2024-01-25 21:20:01 +0300

    added google analytics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {RouterOutlet} from "@angular/router";
-import { NgxGoogleAnalyticsModule } from 'ngx-google-analytics';
+import { NgxGoogleAnalyticsModule, NgxGoogleAnalyticsRouterModule } from 'ngx-google-analytics';
 
 // @NgModule decorator with its metadata
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, RouterOutlet, NgxGoogleAnalyticsModule.forRoot('G-S04M4SEWK6')],
+  imports: [BrowserModule, RouterOutlet, NgxGoogleAnalyticsModule.forRoot('G-S04M4SEWK6'), NgxGoogleAnalyticsRouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
