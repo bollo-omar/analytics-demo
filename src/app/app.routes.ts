a76bbf2fa3f1c56b54ad454a07d5ca30fde47bf9
bollo18 <bolloo18@gmail.com>
2024-01-25 20:34:09 +0300

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {AboutComponent} from "./about/about.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    title:"home",
+    path:"",
+    component: HomeComponent
+  },
+  {
+    title:"about",
+    path:"about",
+    component: AboutComponent
+  }
+];
