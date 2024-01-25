ac342cb75e2edd905cd0a9f0bfb668e9226e2a23
bollo18 <bolloo18@gmail.com>
2024-01-25 21:59:40 +0300

    added google analytics

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,10 +3,6 @@ import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-about',
-  standalone: true,
-  imports: [
-    RouterLink
-  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {RouterOutlet} from "@angular/router";
+import {RouterModule, RouterOutlet} from "@angular/router";
 import { NgxGoogleAnalyticsModule, NgxGoogleAnalyticsRouterModule } from 'ngx-google-analytics';
+import {HomeComponent} from "./home/home.component";
+import {AboutComponent} from "./about/about.component";
+import {routes} from "./app.routes";
 
 // @NgModule decorator with its metadata
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, RouterOutlet, NgxGoogleAnalyticsModule.forRoot('G-S04M4SEWK6'), NgxGoogleAnalyticsRouterModule],
+  declarations: [AppComponent, HomeComponent, AboutComponent],
+  imports: [BrowserModule, RouterOutlet, NgxGoogleAnalyticsModule.forRoot('G-S04M4SEWK6'), NgxGoogleAnalyticsRouterModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent]
 })
